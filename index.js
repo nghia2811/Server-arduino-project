4542f0582bb444957e99be3ead163ac67c9276ab
Nguyen Trung Nghia <katoriitto530@gmail.com>
2020-08-31 21:22:53 +0700

    delete user

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var io = require("socket.io").listen(server);
 var fs = require("fs");
 server.listen(process.env.PORT || 3000);
 
-app.get("/", function(req, res){
-	res.sendFile(__dirname + "/index.html");	
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
 console.log("Server is running!!");
@@ -15,14 +15,14 @@ var arrayUser = [];
 var exist = true;
 
 io.sockets.on('connect', function (socket) {
-    
+
     console.log("Có thiết bị vừa được kết nối! " + socket.id);
-    
+
     // io.sockets.emit('serverguitinnhan', { noidung: "okbaby" });
-    
+
     socket.on('client-register-user', function (data) {
 
-        if(arrayUser.indexOf(data) == -1){
+        if (arrayUser.indexOf(data) == -1) {
             //Ko ton tai user -> dc phep add user
             arrayUser.push(data);
             exist = false;
@@ -31,28 +31,34 @@ io.sockets.on('connect', function (socket) {
             socket.un = data;
 
             ////gui danh sach user ve tat car cac may
-            io.sockets.emit('server-send-userlist', { "danhsach" : arrayUser});
+            io.sockets.emit('server-send-userlist', { "danhsach": arrayUser });
         } else {
             exist = true;
         }
 
         // gui ket qua dang ky nguoi dung -> 1 user
-        socket.emit('server-send-result', { "ketqua" : exist })
+        socket.emit('server-send-result', { "ketqua": exist })
 
-       // emit toi tat ca moi nguoi
-        // io.sockets.emit('server-send-data', { "noidung": data });
-    
     });
 
     socket.on('client-request-userlist', function () {
-            ////gui danh sach user ve tat car cac may
-            io.sockets.emit('server-send-userlist', { "danhsach" : arrayUser});
+        ////gui danh sach user ve tat car cac may
+        io.sockets.emit('server-send-userlist', { "danhsach": arrayUser });
     });
 
     socket.on('client-send-message', function (data) {
         ////gui danh sach user ve tat car cac may
-        io.sockets.emit('server-send-message', { "user" : socket.un,
-                                                "message" : data});
+        io.sockets.emit('server-send-message', {
+            "user": socket.un,
+            "message": data
+        });
+    });
+
+    socket.on('client-exits', function (data) {
+        const index = array.indexOf(data);
+        if (index > -1) {
+            array.splice(index, 1);
+        }
+    });
+
 });
-    
-  });
